4723e40641b07af7c06fda04942951461b13cbad
Raphael Orati <misterbroderkill@gmail.com>
2026-07-07 11:42:05 -0300

    feat: Criação de construtor sem o ID do entity

--- src/main/java/br/com/todo/list/model/Task.java
+++ src/main/java/br/com/todo/list/model/Task.java
@@ -1,14 +1,18 @@
 package br.com.todo.list.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.UUID;
 
 import br.com.todo.list.enums.Prioridade;
 import br.com.todo.list.enums.Status;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
+@Getter
+@Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -17,7 +21,26 @@ public class Task {
     private String descricao;
     private Status status;
     private Prioridade prioridade;
-    private LocalDateTime dataCriacao;
-    private LocalDateTime dataConclusao;
-    private LocalDateTime prazo;
+    private LocalDate dataCriacao;
+    private LocalDate dataConclusao;
+    private LocalDate dataFinalizacao;
+
+    public Task() {
+    }
+
+    public Task(String titulo,
+            String descricao,
+            Status status,
+            Prioridade prioridade,
+            LocalDate dataCriacao,
+            LocalDate dataConclusao,
+            LocalDate dataFinalizacao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.prioridade = prioridade;
+        this.dataCriacao = dataCriacao;
+        this.dataConclusao = dataConclusao;
+        this.dataFinalizacao = dataFinalizacao;
+    }
 }
