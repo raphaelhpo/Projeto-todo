0722f4d50b4074456c93db32ccf4e3594af97657
Raphael Orati <misterbroderkill@gmail.com>
2026-07-12 00:28:23 -0300

    feat: Ajuste e criação de endpoints para integração com frontend

--- src/main/java/br/com/todo/list/controller/TodoListViewController.java
+++ src/main/java/br/com/todo/list/controller/TodoListViewController.java
@@ -1,21 +1,17 @@
 package br.com.todo.list.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.todo.list.dto.create.TaskCreateDTO;
-import br.com.todo.list.dto.response.TaskResponseDTO;
 import br.com.todo.list.service.TodoListService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
-@RequestMapping("todolist/")
+@RequestMapping("todolist")
 public class TodoListViewController {
     TodoListService service;
 
@@ -23,31 +19,16 @@ public class TodoListViewController {
         this.service = service;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String home(Model model) {
-        return "main";
-    }
-
-    @GetMapping("tasks")
-    public String listar(Model model) {
-        List<TaskResponseDTO> tasks = service.consultarTodasTasks();
-        model.addAttribute("tasks", tasks);
+        model.addAttribute("tasks", service.consultarTodasTasks());
         model.addAttribute("taskForm", new TaskCreateDTO());
-        return "tasks/listar";
+        return "lista_tasks";
     }
 
-    // exibe o formulário
-    @GetMapping("/novo")
-    public String novo(Model model) {
-        model.addAttribute("taskForm", new TaskCreateDTO());
-        return "tasks/form";
-    }
-
-    // recebe os dados do formulário
     @PostMapping("/criar")
-    public String criar(@ModelAttribute TaskCreateDTO task) {
+    public String postPageCriar(@ModelAttribute TaskCreateDTO task) {
         service.criarTask(task);
-        return "redirect:/tasks";
+        return "redirect:/todolist";
     }
-
 }
