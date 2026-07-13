9adcbaea8560461bf3ce4e755c8ee975788fad5b
Raphael Orati <misterbroderkill@gmail.com>
2026-07-12 21:09:57 -0300

    feat: Novo DTO para realização de Update, inclusão do mesmo no mapper.

--- src/main/java/br/com/todo/list/mapper/TaskMapper.java
+++ src/main/java/br/com/todo/list/mapper/TaskMapper.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import br.com.todo.list.dto.create.TaskCreateDTO;
 import br.com.todo.list.dto.response.TaskResponseDTO;
+import br.com.todo.list.dto.update.TaskUpdateDTO;
 import br.com.todo.list.enums.Status;
 import br.com.todo.list.model.Task;
 
@@ -30,9 +31,46 @@ public class TaskMapper {
         task.setPrioridade(dto.getPrioridade());
         task.setDataFinalizacao(dto.getDataFinalizacao());
         task.setDataConclusao(null);
-        // valores fixos definidos aqui, não no construtor do DTO
         task.setStatus(Status.PENDENTE);
         task.setDataCriacao(LocalDate.now());
         return task;
+    }
+
+    public static Task toEntity(TaskResponseDTO dto) {
+        Task task = new Task();
+        task.setId(dto.getId());
+        task.setTitulo(dto.getTitulo());
+        task.setDescricao(dto.getDescricao());
+        task.setPrioridade(dto.getPrioridade());
+        task.setDataFinalizacao(dto.getDataFinalizacao());
+        task.setDataConclusao(dto.getDataConclusao());
+        task.setStatus(dto.getStatus());
+        task.setDataCriacao(dto.getDataCriacao());
+        return task;
+    }
+
+    public static Task toEntity(TaskUpdateDTO dto) {
+        Task task = new Task();
+        task.setId(dto.getId());
+        task.setTitulo(dto.getTitulo());
+        task.setDescricao(dto.getDescricao());
+        task.setPrioridade(dto.getPrioridade());
+        task.setDataFinalizacao(dto.getDataFinalizacao());
+        task.setDataConclusao(dto.getDataConclusao());
+        task.setStatus(dto.getStatus());
+        task.setDataCriacao(dto.getDataCriacao());
+        return task;
+    }
+
+    public static TaskCreateDTO toCreate(TaskResponseDTO task) {
+        TaskCreateDTO taskDto = new TaskCreateDTO();
+        taskDto.setTitulo(task.getTitulo());
+        taskDto.setDescricao(task.getDescricao());
+        taskDto.setStatus(task.getStatus());
+        taskDto.setPrioridade(task.getPrioridade());
+        taskDto.setDataCriacao(task.getDataCriacao());
+        taskDto.setDataConclusao(task.getDataConclusao());
+        taskDto.setDataFinalizacao(task.getDataFinalizacao());
+        return taskDto;
     }
 }
